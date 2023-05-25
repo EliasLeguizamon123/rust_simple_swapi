9837b145f2a0eb7ae4eab740d3dc985f2726d03b
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2023-05-25 14:18:25 -0300

    first commit

---
+++ src/bin/main.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize};
+use reqwest::{Error};
+
+#[derive(Deserialize, Debug)]
+struct Person {
+    name: String,
+}
+#[derive(Deserialize, Debug)]
+struct PeopleResponse {
+    results: Vec<Person>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = "https://swapi.dev/api/people/";
+
+    let response = reqwest::get(url).await?;
+    let people : Vec<Person> = response.json::<PeopleResponse>().await?.results;
+
+    for person in people {
+        println!("Name: {}", person.name);
+    }
+
+    Ok(())
+}
